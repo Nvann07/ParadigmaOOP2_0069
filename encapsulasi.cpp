a968b6a234012c97e923b0915f3cef0748890ab4
Nvann07 <nrizwardi@gmail.com>
2025-06-03 14:57:42 +0700

    Membuat fungsi main untuk menjalankan  perogram

--- encapsulasi.cpp
+++ encapsulasi.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-class remotLampu {
+class remoteLampu {
 private:
 string saklarNo[10];
 public:
@@ -14,5 +14,17 @@ string getSaklarNo(int i) {
 };
 
 int main() {
-  
+  remoteLampu lampuRumah;
+
+  lampuRumah.setSaklarNo(0, "Lampu Teras Rumah");
+  lampuRumah.setSaklarNo(1, "Lampu Ruang Tamu ");
+  lampuRumah.setSaklarNo(2, "Lampu Kamar Tidur");
+  lampuRumah.setSaklarNo(3, "Lampu Dapur ");
+
+  cout << lampuRumah.getSaklarNo(0) << endl;
+  cout << lampuRumah.getSaklarNo(1) << endl;
+  cout << lampuRumah.getSaklarNo(2) << endl;
+  cout << lampuRumah.getSaklarNo(3) << endl;
+
+  return 0;
 }
