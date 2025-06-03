a88a421c2253bab5c15ba1b0baaeef9fecb7e26c
Nvann07 <nrizwardi@gmail.com>
2025-06-03 14:53:48 +0700

    Membuat class remotlampu pada encapulasi

---
+++ encapsulasi.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class remotLampu {
+private:
+string saklarNo[10];
+public:
+void setSaklarNo(inyt i, string value) {
+  setSaklarNo[i] = value;
+}
+string getSaklarNo(int i) {
+  return saklarNo[i];
+}
+};
+
+int main() {
+  
+}
